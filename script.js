f98d42dbda3c3437d3c95d4a7a8929f2e0af120d
Roman Osborne - LDE Learner <rosbornebusiness@protonmail.com>
2024-10-01 13:15:08 +0100

    UpdateYey <3

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("scroll", function() {
     const container = document.querySelector("#front");
     const vignette = document.querySelector("#vignette");
@@ -88,3 +87,137 @@ window.addEventListener('resize', () => {
     console.log('Updated Element Width:', elementWidth, 'px');
     console.log('Updated Element Height:', elementHeight, 'px');
 });
+
+$(document).ready(function() {
+    var counter = 5;
+    var left = $('#left')
+    var right = $('#right')
+    $(right).click(function() {
+    $(left).prop('disabled', true);
+    $(right).prop('disabled', true);
+    if (counter == 8) {
+        $('#item8').removeClass('trans');
+        $('#item1').removeClass('trans');
+        $('.carousel-track').removeClass('trans');
+    } else if (counter == 1) {
+        counter = 8;
+        $(`#item7, #item8`).css({ visibility: 'visible' });
+            $(`#item8`).addClass('trans').css({ width: "20vw", height: "68vh", transform: 'translateY(0vh)' });
+            $(`#item9, #item6`).css({ visibility: 'visible' });
+            $(`#item1`).addClass('trans').css({ width: "15vw", height: "50vh", transform: 'translateY(8vh)' });
+            $(`.carousel-track`).addClass('trans').css({ transform: 'translateX(-57.65%)' });
+            $(`#item0, #item1, #item2`).css({ visibility: 'hidden' });
+    }
+    var push = (((counter-6) * - 9.714285714) -28.50714258714);
+    $(`#item${counter}`).animate({ width: "15vw", height: "50vh" }, 200);
+    $(`#item${counter - 1}`).animate({ width: "20vw", height: "68vh" }, 200);
+    $(`#item${counter - 2}`).css({ visibility: 'visible' });
+    $('.carousel-track').css({ transform: `translateX(${push}%)`});
+    $(`#item${counter}`).css({ transform: 'translateY(8vh)' });
+    $(`#item${counter - 1}`).css({ transform: 'translateY(0vh)' });
+    $(`.carousel-track`).removeClass('trans');
+    $('#item1').removeClass('trans');
+    $('#item8').removeClass('trans');
+    setTimeout(function() {
+        $(left).prop('disabled', false);
+        $(right).prop('disabled', false);
+        $(`#item${counter + 1}`).css({ visibility: 'hidden' });
+        counter--;
+        if (counter < 2) {
+            counter = 8;
+            $(`#item7, #item8`).css({ visibility: 'visible' });
+            $(`#item8`).addClass('trans').css({ width: "20vw", height: "68vh", transform: 'translateY(0vh)' });
+            $(`#item9`).css({ visibility: 'visible' });
+            $(`#item1`).addClass('trans').css({ width: "15vw", height: "50vh", transform: 'translateY(8vh)' });
+            $(`.carousel-track`).addClass('trans').css({ transform: 'translateX(-57.65%)' });
+            $(`#item0, #item1, #item2`).css({ visibility: 'hidden' });
+        }
+    }, 1000);
+});
+
+    $(left).click(function() {
+        $(left).prop('disabled', true);
+        $(right).prop('disabled', true);
+        if (counter == 1) {
+            $('#item8').removeClass('trans');
+            $('#item1').removeClass('trans');
+            $('.carousel-track').removeClass('trans');
+        } else if (counter == 8) {
+        counter = 1;
+        $(`#item0, #item1`).css({ visibility: 'visible' });
+                $('#item1').addClass('trans').css({ width: "20vw", height: "68vh", transform: 'translateY(0vh)' });
+                $(`#item2`).css({ visibility: 'visible' });
+                $('.carousel-track').addClass('trans').css({ transform: 'translateX(10.35%)'});
+                $(`#item7, #item8, #item9`).css({ visibility: 'hidden' });
+                $(`#item8`).css({ width: "15vw", height: "50vh", transform: 'translateY(8vh)' });
+    }
+        var push = (((counter - 4) * - 9.714285714) -28.50714258714);
+        $(`#item${counter}`).animate({ width: "15vw", height: "50vh" }, 200);
+        $(`#item${counter + 1}`).animate({ width: "20vw", height: "68vh" }, 200);
+        $(`#item${counter + 2}`).css({ visibility: 'visible' });
+        $('.carousel-track').css({ transform: `translateX(${push}%)`});
+        $(`#item${counter + 1}`).css({ transform: 'translateY(0vh)' });
+        $(`#item${counter}`).css({ transform: 'translateY(8vh)' });
+        $(`.carousel-track`).removeClass('trans');
+        $('#item1').removeClass('trans');
+        $('#item8').removeClass('trans');
+        setTimeout(function() {
+            $(left).prop('disabled', false);
+            $(right).prop('disabled', false);
+            $(`#item${counter - 1}`).css({ visibility: 'hidden' });
+            counter++;
+            if (counter > 7) {
+                counter = 1;
+                $(`#item0, #item1`).css({ visibility: 'visible' });
+                $('#item1').addClass('trans').css({ width: "20vw", height: "68vh", transform: 'translateY(0vh)' });
+                $(`#item2`).css({ visibility: 'visible' });
+                $('.carousel-track').addClass('trans').css({ transform: 'translateX(10.4%)' });
+                $(`#item7, #item8, #item9`).css({ visibility: 'hidden' });
+                $(`#item8`).css({ width: "15vw", height: "50vh", transform: 'translateY(8vh)' });
+            }
+        }, 1000);
+    });
+});
+function unit4(){
+    location.assign("website-development.html");
+}
+function unit10(){
+    location.assign("animation.html");
+}
+function unit21(){
+    location.assign("pitching-a-product.html");
+}
+function adjustTranslateY() {
+            const div = document.getElementById('vignette');
+            const screenWidth = window.innerWidth;
+
+            // Change the translateY value based on screen width
+            let translateYValue;
+
+            if (screenWidth >= 1800) {
+                translateYValue = -20;
+            } else if (screenWidth >= 1600) {
+                translateYValue = -31;
+            } else if (screenWidth >= 1400) {
+                translateYValue = -41;
+            } else if (screenWidth >= 1200) {
+                translateYValue = -51;
+            } else if (screenWidth >= 1000) {
+                translateYValue = -61;
+            } else if (screenWidth >= 800) {
+                translateYValue = -71;
+            } else if (screenWidth >= 600) {
+                translateYValue = -81;
+            } else if (screenWidth >= 400) {
+                translateYValue = -91;
+            } else {
+                translateYValue = -101;
+            }
+
+            // Apply the transformation
+            div.style.transform = `translate(100vw ,${translateYValue}%)`;
+        }
+
+        // Listen for window resize and load events
+        window.addEventListener('resize', adjustTranslateY);
+        window.addEventListener('load', adjustTranslateY);
