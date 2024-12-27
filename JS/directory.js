340d7975c4db38cf5c218d900353290941431364
the-romanov <rosbornebusiness@protonmail.com>
2024-12-27 12:21:12 +0000

    Take 2 Directory

--- JS/directory.js
+++ JS/directory.js
@@ -1,18 +1,18 @@
 function websitedevelopment() {
-    location.assign("./Coursework/website-development.html");
+    location.assign("Portfolio/Coursework/website-development.html");
 }
 function animation() {
-    location.assign("./Coursework/animation.html");
+    location.assign("Portfolio/Coursework/animation.html");
 }
 function modelling() {
-    location.assign("./Coursework/3d-modelling.html");
+    location.assign("Portfolio/Coursework/3d-modelling.html");
 }
 function gamedevelopment() {
-    location.assign("./Coursework/game-development");
+    location.assign("Portfolio/Coursework/game-development");
 }
 function pitchingaproduct() {
-    location.assign("./Coursework/pitching-a-product.html");
+    location.assign("Portfolio/Coursework/pitching-a-product.html");
 }
 function amnesic() {
-    location.assign("./Projects/amnesic.html");
+    location.assign("Portfolio/Projects/amnesic.html");
 }
