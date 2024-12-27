4a0e507cf4784a1755f9b02d6d76e6ef865ad600
the-romanov <rosbornebusiness@protonmail.com>
2024-12-27 12:15:40 +0000

    Directory Fix

--- JS/directory.js
+++ JS/directory.js
@@ -1,18 +1,18 @@
 function websitedevelopment() {
-    location.assign("/Coursework/website-development.html");
+    location.assign("./Coursework/website-development.html");
 }
 function animation() {
-    location.assign("/Coursework/animation.html");
+    location.assign("./Coursework/animation.html");
 }
 function modelling() {
-    location.assign("/Coursework/3d-modelling.html");
+    location.assign("./Coursework/3d-modelling.html");
 }
 function gamedevelopment() {
-    location.assign("/Coursework/game-development");
+    location.assign("./Coursework/game-development");
 }
 function pitchingaproduct() {
-    location.assign("/Coursework/pitching-a-product.html");
+    location.assign("./Coursework/pitching-a-product.html");
 }
 function amnesic() {
-    location.assign("/Projects/amnesic.html");
+    location.assign("./Projects/amnesic.html");
 }
